c1778eaf7170ff6c913226ae385f11d127742ec7
miobioha <miobioha@gmail.com>
2016-01-18 12:27:17 +0000

    Improved performance.
    Re-factored word counting algorithm so that it is more memory efficient.

--- WordCounter.Core/StringExtension.cs
+++ WordCounter.Core/StringExtension.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using WordCounter.Core.Enums;
 
 namespace WordCounter.Core
@@ -18,33 +17,81 @@ namespace WordCounter.Core
     /// <remarks>Word compare is case insensitive for thisi method.</remarks>
     public static Dictionary<string, int> CountDistinctWords(this string str, params char[] punctuations)
     {
-      return CountDistinctWordsInternal(str, WordCountOptions.CaseInsensitive, punctuations);
+      return CountDistinctWords(str, WordCountOptions.CaseInsensitive, punctuations);
     }
 
-    public static Dictionary<string, int> CountDistinctWords(this string str, WordCountOptions options, 
+    public static Dictionary<string, int> CountDistinctWords(this string str, WordCountOptions options,
       params char[] punctuations)
     {
-      return CountDistinctWordsInternal(str, options, punctuations);
+      return CountDistinctWordsInternal2(str, options, punctuations);
     }
 
-    private static Dictionary<string, int> CountDistinctWordsInternal(string str, WordCountOptions options, 
+    private static Dictionary<string, int> CountDistinctWordsInternal2(string str, WordCountOptions options,
       params char[] punctuations)
     {
-      str.ThrowIfNull(nameof(str));
+      str.ThrowIfNull("str");
       punctuations = punctuations.Length != 0 ? punctuations : Punctuations.Default;
-      var words = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
       Dictionary<string, int> distinctWords = new Dictionary<string, int>();
-
-      for (int i = 0, j = words.Length; i < j; i++)
+      var pset = new HashSet<char>(punctuations);
+      int start = -1;
+      for (int i = 0; i < str.Length; i++)
       {
-        string word = words[i].TrimStart(punctuations).TrimEnd(punctuations);
-        if (options == WordCountOptions.CaseInsensitive) { word = word.ToLowerInvariant();}
-        if (word == string.Empty) { continue; }
-        int count = distinctWords.ContainsKey(word) ? distinctWords[word] : 0;
-        distinctWords[word] = ++count;
+        if (char.IsWhiteSpace(str[i]))
+        {
+          if (start != -1)
+          {
+            int end = i - 1;
+            CountWord(GetWord(str, start, end, pset), distinctWords, options);
+            start = -1;
+          }
+
+          continue;
+        }
+
+        if (start != -1 && i == str.Length - 1)
+        {
+          int end = str.Length - 1;
+          CountWord(GetWord(str, start, end, pset), distinctWords, options);
+          start = -1;
+
+          continue;
+        }
+
+        if (start == -1)
+        { start = i; }
       }
 
       return distinctWords;
+    }
+
+    /// <summary>
+    /// Get word from specifed string, removing any surrounding punctuations specified in the punctuation set.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="start">The start character position.</param>
+    /// <param name="end">The end character position.</param>
+    /// <param name="pset"></param>
+    /// <returns>The extracted word if any otherwise empty string.</returns>
+    private static string GetWord(string str, int start, int end, HashSet<char> pset)
+    {
+      while (start <= end && pset.Contains(str[start]))
+      { start++; }
+
+      while (end >= start && pset.Contains(str[end]))
+      { end--; }
+
+      if (start > end)
+      { return string.Empty; }
+
+      return str.Substring(start, end - start + 1);
+    }
+
+    private static void CountWord(string word, Dictionary<string, int> words, WordCountOptions options)
+    {
+      if (word == string.Empty) { return; }
+      if (options == WordCountOptions.CaseInsensitive) { word = word.ToLowerInvariant(); }
+      int count = words.ContainsKey(word) ? words[word] : 0;
+      words[word] = ++count;
     }
   }
 }
--- readme.txt
+++ readme.txt
@@ -12,3 +12,5 @@ so - 1
 The result shows the comparsion is case-insensitive therefore the provided solution will do a case-insensitive 
 compare. An acceptance test that verifies the given specification is included in this solution and is executed 
 with other tests. The solution is using a small standard set of punctuations that can be easily extended.
+
+Standard Set of Punctuations = { '.', ';', ':', '(', ')', '{', '}', '\'', '\"', '\n', '!', '?', ',', '-', '[', ']', '“', '”' }
